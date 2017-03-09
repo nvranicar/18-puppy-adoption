7b7d5a6a434ae67f6ce2a8e419d7bb2f8596a3e8
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-03-09 15:13:29 -0600

    started styling nav bar

---
+++ app/routes/application.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="">
+    <nav class="nav">
+      <div class="nav-left">
+        <div class="nav-item">
+          <h2>Puppies</h2>
+        </div>
+      </div>
+      <div class="nav-right">
+        <div class="nav-item">
+          <router-link :to="{ name: 'index' }">All Puppies</router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
