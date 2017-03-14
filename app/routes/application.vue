02aad02b6f603bb7ba6972ce3b822610ec04bfe4
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-03-14 14:38:32 -0500

    some styling

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -12,6 +12,26 @@
         </div>
       </div>
     </nav>
+    <div class="section">
+      <div class="container">
+        <div class="sidebar box">
+          <div class="sidebar__heading">
+            <h2>Adopt a Pupper</h2>
+          </div>
+            <div class="level">
+              <div class="level-left">
+                <div class="level-item">
+                  <img src="http://placecorgi.com/300/200" alt="" class="pup-pic">
+                </div>
+                <div class="level-item">
+                  <h2>Pup Name</h2>
+                  <router-link>read more</router-link>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
